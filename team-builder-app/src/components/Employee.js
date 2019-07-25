aa65e97114fb6101cda5f48c18a3a30ecb48a9d0
Joscelyn <JoscelynRose@gmail.com>
2019-07-25 10:11:45 -0400

    added icons

--- team-builder-app/src/components/Employee.js
+++ team-builder-app/src/components/Employee.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Card, List } from "semantic-ui-react";
+import { Card, List, Icon } from "semantic-ui-react";
 
 function Employee(props) {
   return (
     <Card className="employee-card">
       <Card.Content>
+        <Card.Header id="buttons">
+          <Icon name="edit" />
+          <Icon name="close" />
+        </Card.Header>
         <Card.Header>{props.name}</Card.Header>
         <List>
           <List.Item>Role: {props.role}</List.Item>
